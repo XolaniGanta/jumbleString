d132a7dc2515767dfd01ec0b304ed1bbe2e19c50
xolanigAPS <xolanig@aps.holdings>
2022-10-07 11:59:51 +0200

    V1

---
+++ src/jumbled.java
@@ -0,0 +1,29 @@
+public class jumbled {
+
+        public static String jumbledString(String s, Long n) {
+            StringBuilder sbEven = new StringBuilder();
+            StringBuilder sbOdd = new StringBuilder();
+            char[] word = s.toCharArray();
+            if (n > 0) {
+                for (int x = 0; x < n; x++) {
+                    word = newJumble(word, sbEven, sbOdd);
+                }
+            }
+            System.out.println(word);
+            return word.toString();
+        }
+        private static char[] newJumble(char[] s, StringBuilder sbEven, StringBuilder sbOdd) {
+            sbEven.setLength(0);
+            sbOdd.setLength(0);
+            StringBuilder result;
+            for (int i = 0; i < s.length; i++) {
+                if (i % 2 == 0) {
+                    sbEven.append(s[i]);
+                } else {
+                    sbOdd.append(s[i]);
+                }
+            }
+            result = sbEven.append(sbOdd);
+            return result.toString().toCharArray();
+        }
+}
